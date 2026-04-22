8dc4a40a921b52f3a41384fb85a5945f00a1e942
Sankarshana  Nanjangud <107449339+Sankarshana02@users.noreply.github.com>
2026-04-22 15:16:09 -0700

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        int xorr=nums[0];
+        for(int i=1;i<n;i++){
+            xorr^=nums[i];
+        }
+
+        return xorr;
+        
+    }
+};
